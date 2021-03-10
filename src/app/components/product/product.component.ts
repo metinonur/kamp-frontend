0590c381a364b9c9957419293988749e9fbad448
Metin Onur <metinonur09@gmail.com>
2021-03-10 22:44:41 +0300

    .

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductResponseModel } from 'src/app/models/productResponseModel';
 import { Product } from 'src/app/models/products';
-
+import {HttpClient} from '@angular/common/http'
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
@@ -11,12 +11,16 @@ export class ProductComponent implements OnInit {
 
 
   products:Product[] = [];
+  apiUrl ="https://localhost:44377/api/products/getall"
   //productResponseModel:ProductResponseModel=();
-
-  constructor() { }
+  constructor(private httpClient:HttpClient){}
 
   ngOnInit(): void {
     console.log("Init çalıştı")
+  }
+
+  getProducts(){
+    this.httpClient.get()
   }
 
 }
