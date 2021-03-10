bdbe974aa76b2063c5bd2c8e3a60a1244e8d1909
Metin Onur <metinonur09@gmail.com>
2021-03-11 00:12:40 +0300

    .

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductResponseModel } from 'src/app/models/productResponseModel';
 import { Product } from 'src/app/models/products';
-import {HttpClient} from '@angular/common/http'
+import { ProductService } from 'src/app/services/product.service';
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
@@ -11,19 +11,21 @@ export class ProductComponent implements OnInit {
 
 
   products:Product[] = [];
-  apiUrl ="https://localhost:44377/api/products/getall"
-  constructor(private httpClient:HttpClient){}
+  dataLoaded=false;
+ 
+  constructor(private productService:ProductService){}
 
   ngOnInit(): void {
     this.getProducts();
   }
 
   getProducts(){
-    this.httpClient
-    .get<ProductResponseModel>(this.apiUrl)
-    .subscribe((response) =>{
-      this.products = response.data
+    this.productService.getProducts().subscribe(response=>{
+      this.products=response.data
+      this.dataLoaded=true;
     })
+    
+    }
   }
 
-}
+
